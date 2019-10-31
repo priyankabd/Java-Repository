88d7187aff8223b06a68b737be61248ef15ac765
priyankabd <41136427+priyankabd@users.noreply.github.com>
2019-10-31 04:14:52 -0400

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,26 @@
+
+ class Product {
+
+	 public static void prod1(int a,int b) 
+	 {
+		 System.out.println(a*b);
+     }
+	 
+	 public static void prod2(double a,double b) 
+	 {
+		 System.out.println(a*b);
+     }
+	 public static void prod3(int a, int b,int c) 
+	 {
+		 System.out.println(a*b*c);
+     }
+
+	      
+	public static void main(String[] args) {
+		prod1(10,20);
+		prod2(2.5,1.3);
+		prod3(1,2,3);
+
+	}
+
+}
